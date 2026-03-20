6d03fc9d7e9b5c5e25c31e2f09685b055a236ac4
Bhoomithakur26 <thakurbhoomi2626@gmail.com>
2026-03-20 09:06:30 +0530

    UC12 added

--- app/src/BookMyStay.java
+++ app/src/BookMyStay.java
@@ -587,3 +587,40 @@ class ConcurrentBookingProcessor extends Thread {
         }
     }
 }
+class PersistenceService {
+
+    private static final String FILE_NAME = "inventory.dat";
+
+    // Save inventory to file
+    public void saveInventory(RoomInventory inventory) {
+
+        try (ObjectOutputStream out =
+                     new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+
+            out.writeObject(inventory.getAllAvailability());
+
+            System.out.println("Inventory saved successfully.");
+
+        } catch (IOException e) {
+            System.out.println("Error saving inventory: " + e.getMessage());
+        }
+    }
+
+    // Load inventory from file
+    public Map<String, Integer> loadInventory() {
+
+        try (ObjectInputStream in =
+                     new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+
+            return (Map<String, Integer>) in.readObject();
+
+        } catch (Exception e) {
+
+            System.out.println("No valid inventory data found. Starting fresh.");
+            return null;
+        }
+    }
+}
+public Map<String, Integer> getAllAvailability() {
+    return availability;
+}
